b17d718133c6ea7aef94d63de3bf3a30bb06906b
Jorge DC <jddelcampom@gmail.com>
2024-07-02 11:18:28 -0500

    Add user validation middleware in /user post endpoint

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import { createUser } from '../controllers/user.controller';
+import { userValidatorCreate } from '../validators/user.validator';
 
 const router = express.Router();
 
-router.post('/user', createUser);
+router.post('/user', userValidatorCreate, createUser);
 
 export default router
