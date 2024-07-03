b553f23be33eca097b02083dcd270c1fe104f01b
Diego Reyes <diegorele13@gmail.com>
2024-07-03 16:47:49 -0500

    add: controller to get name.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,6 +6,7 @@ import models from '../models';
 
 export async function createUser(req: Request, res: Response): Promise<void> {
   try {
+    console.log('pasamos por aqui')
     const { from: number }: Ctx = req.body.ctx;
 
     const existingUser = await models.user.findOne({ cellphone: number });
@@ -30,7 +31,7 @@ export async function createUser(req: Request, res: Response): Promise<void> {
 
 export async function setUserName(req: Request, res: Response): Promise<void> {
   try {
-    const { from: number, body: message }: Ctx = req.body;
+    const { from: number, body: message }: Ctx = req.body.ctx;
     
     const user = await models.user.findOne({cellphone: number});
 
@@ -38,10 +39,28 @@ export async function setUserName(req: Request, res: Response): Promise<void> {
       return handleHttpError(res, 'user not found');
     };
 
-    user.name = message;
-    await user.save();
+    let responseMessage: string;
 
-    res.status(200).send({message: 'user name saved'})
+    const hasFirstNameAndLastName = /\b\w+\b\s+\b\w+\b/.test(message);
+
+    if (hasFirstNameAndLastName) {
+      user.name = message;
+      await user.save();
+      responseMessage = 'Se ha guardado exitosamente tu nombre.';
+    } else {
+      responseMessage = 'El nombre debe contener al menos un nombre y un apellido.';
+    };
+
+    const response = {
+      messages: [
+        {
+          type: 'to_user', 
+          content: responseMessage
+        }
+      ]
+    };
+
+    res.status(200).send(response)
   } catch (error) {
     handleHttpError(res, 'cannot get user name');
   }
