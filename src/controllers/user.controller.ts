c387bfec8a8ecdb0f0de6ff7522ad2cb80270657
Jorge DC <jddelcampom@gmail.com>
2024-07-03 20:27:14 -0500

    ADD: user verify cuit organizations endpoint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -49,8 +49,8 @@ export async function setUserName(req: Request, res: Response): Promise<void> {
 
 export async function getBenefitNumber(req: Request, res: Response) {
   try {
-    const { from: number, body: message }: Ctx = req.body;
-    
+    const { from: number, body: message }: Ctx = req.body.ctx;
+
     const user = await models.user.findOne({cellphone: number});
     
     if (!user) {
@@ -69,3 +69,34 @@ export async function getBenefitNumber(req: Request, res: Response) {
     handleHttpError(res, 'cannot get benefit number')
   }
 }
+
+export async function verifyCuitOrganizations(req: Request, res: Response) {
+  try {
+   const { from: number }: Ctx = req.body.ctx;
+ 
+   const user = await models.user.findOne({cellphone: number});
+ 
+   if (!user) {
+     return handleHttpError(res, 'user not found');
+   };
+ 
+   if(!user.CUIT) {
+     return handleHttpError(res, 'user cuit not found')
+   }
+
+   const responseMessage = `🔍 Verifícanos por favor si este es el organismo que te paga los haberes: ${user.CUIT} ✅`
+
+   const response = {
+    messages: [
+      {
+        type: 'to_user',
+        content: responseMessage,
+      }
+    ]
+   }
+ 
+   res.status(200).send(response)
+  } catch (error) {
+    handleHttpError(res, 'cannot verify cuit organizations')
+  } 
+ }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createUser, setUserName } from '../controllers/user.controller';
+import { createUser, setUserName, getBenefitNumber, verifyCuitOrganizations } from '../controllers/user.controller';
 import { userValidatorCreate } from '../validators/user.validator';
 
 const router = express.Router();
@@ -8,5 +8,9 @@ const router = express.Router();
 router.post('/user', userValidatorCreate, createUser);
 
 router.post('/set-user-name', setUserName);
+
+router.get('/get-benefit-number', getBenefitNumber);
+
+router.get('/verify-cuit-organizations', verifyCuitOrganizations);
 
 export default router;
