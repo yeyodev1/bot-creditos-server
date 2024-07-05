8156885cd46a1260ebbc8e754d3e73d5512f20e9
Diego Reyes <diegorele13@gmail.com>
2024-07-05 14:06:00 -0500

    add: route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import express, { Application } from 'express';
 
-import user from './user'
+import user from './user';
+import credit from './credit';
 
 function routerApi(app: Application) {
   const router = express.Router();
 
   app.use('/api', router);
 
-  router.use(user)
-}
+  router.use(user);
+  router.use(credit);
+};
 
 export default routerApi
