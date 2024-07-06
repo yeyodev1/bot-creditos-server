7b9f614097765dceb545539b51bf80ebe35f98c6
Jorge DC <jddelcampom@gmail.com>
2024-07-06 14:49:50 -0500

    REFACTOR: change get routes for post

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -13,14 +13,13 @@ import { userValidatorCreate } from '../validators/user.validator';
 
 const router = express.Router();
 
-router.post('/');
 router.post('/set-user-name', setUserName);
 router.post('/set-user-cuil', setUserCuil);
 router.post('/set-user-email', setUserEmail);
 router.post('/user', userValidatorCreate, createUser);
 router.post('/set-user-benefit-number', setBenefitNumber);
 
-router.get('/get-benefit-number', getBenefitNumber);
-router.get('/verify-cuit-organizations', verifyCuitOrganizations);
+router.post('/get-benefit-number', getBenefitNumber);
+router.post('/verify-cuit-organizations', verifyCuitOrganizations);
 
 export default router;
