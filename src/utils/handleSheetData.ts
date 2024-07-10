e421779cdd071c1b02187e75f16c051641112451
Jorge DC <jddelcampom@gmail.com>
2024-07-09 22:34:43 -0500

    ADD: utility function to handle sheet data and update addRowsToSheet

--- src/utils/handleSheetData.ts
+++ src/utils/handleSheetData.ts
@@ -4,12 +4,22 @@ import { getSheetByIndex } from "../services/SheetService";
 type RowCellData = string | number | boolean | Date;
 type RowData = Record<string, RowCellData>;
 
-interface AddRowsToSheetParams {
-  sheetIndex?: number;
-  data: RowData[];
+export const objectDataSheet: RowData = {
+  nombre: '',
+  cuil: '',
+  cuit: '',
+  email: '',
+  "codigo de area": '',
+  "resto del numero": '',
+  "nro de beneficio": '',
+  "foto de anverso dni": '',
+  "foto de verso dni": '',
+  "ultimo recibo de haberes": '',
 }
 
-export async function addRowsToSheet({sheetIndex, data}: AddRowsToSheetParams): Promise<GoogleSpreadsheetRow<Record<string, any>>[]>{
+export async function addRowsToSheet(sheetIndex?: number): Promise<GoogleSpreadsheetRow<Record<string, any>> | undefined>{
   const sheet = await getSheetByIndex(sheetIndex);
-  return sheet.addRows(data);
+  const isValidObject = Object.entries(objectDataSheet).every(([_, value]) => value !== '');
+
+  if(isValidObject) return sheet.addRow(objectDataSheet);
 }
